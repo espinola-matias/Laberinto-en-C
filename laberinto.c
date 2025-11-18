019bbf8e773b2bd3f894070f834098bf995f825f
matiasespinola <rojasmati98@gmail.com>
2025-11-18 16:15:40 -0300

    defino los parametros

--- laberinto.c
+++ laberinto.c
@@ -2,3 +2,12 @@
 #include <stdbool.h>  
 #include <stdlib.h>   
 #include <time.h>
+
+#define DIMENSION 11
+#define SOLUCION '-'
+#define MUROS '#'
+#define CAMINO ' '
+#define ENTRADA 'E'
+#define SALIDA 'S'
+
+char laberinto_tablero[DIMENSION][DIMENSION];
