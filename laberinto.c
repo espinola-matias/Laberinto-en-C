30f05ea8285ce6580f7b1bffa8b2da8f960804a2
matiasespinola <rojasmati98@gmail.com>
2025-11-18 16:18:07 -0300

    Anunciando las funciones antes del main

--- laberinto.c
+++ laberinto.c
@@ -11,3 +11,11 @@
 #define SALIDA 'S'
 
 char laberinto_tablero[DIMENSION][DIMENSION];
+bool visitados[DIMENSION][DIMENSION];
+
+// declaraciones de funciones a utilizar 
+void inicializar_tablero();
+void imprimir_tablero();
+bool resolver_tablero(int fila_actual, int columna_actual);
+void generar_laberinto(int fila_actual, int columna_actual);
+void mezclar(int arr[4][2]);
