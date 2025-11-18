ee6f571706fd4f0d38233668f9a1d2123859decc
matiasespinola <rojasmati98@gmail.com>
2025-11-18 16:21:24 -0300

    creo un tablero lleno de muros

--- laberinto.c
+++ laberinto.c
@@ -19,3 +19,27 @@ void imprimir_tablero();
 bool resolver_tablero(int fila_actual, int columna_actual);
 void generar_laberinto(int fila_actual, int columna_actual);
 void mezclar(int arr[4][2]);
+
+int main() {
+    srand(time(NULL)); // inicializo y utiliza el tiempo actual como semilla 
+
+    inicializar_tablero();
+    generar_laberinto(0, 0);
+    laberinto_tablero[0][0] = ENTRADA;
+    imprimir_tablero();
+    clock_t tiempo_inicio = clock();   // utiliza ticks y guarda en el momento de la ejecucion 
+
+    bool resuelto = resolver_tablero(0, 0);
+    laberinto_tablero[DIMENSION -1][DIMENSION -1] = SALIDA;
+
+    if (resuelto == false) {
+        printf("No se encontro la salida!\n");
+    } else {
+        printf("Se encontro la solucion \n");
+        clock_t tiempo_fin = clock();  
+        double tiempo_total = ((double)(tiempo_fin - tiempo_inicio)) / CLOCKS_PER_SEC;  // dividimos los ticks para obtener en segundos reales 
+        printf("Tiempo de resolucion: %f segundos\n", tiempo_total);
+        imprimir_tablero();
+    }
+    return 0;
+}
